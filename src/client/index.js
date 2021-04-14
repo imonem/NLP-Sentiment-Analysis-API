32b2f3c0b7c12ae004d84c0d02df78a9694ccbc6
Amr Nabeel <amr.nabeel@gmail.com>
2021-04-14 03:31:00 +0200

    removed redundant files

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,4 @@
-// import { handleSubmit } from './js/formHandler';
 import { getMeaning } from './js/formHandler';
-import { checkForName } from './js/nameChecker';
 import babelPolyfill from 'babel-polyfill';
 import './styles/resets.scss';
 import './styles/base.scss';
@@ -9,11 +7,8 @@ import './styles/form.scss';
 import './styles/header.scss';
 import './styles/colors.scss';
 
-// console.log(checkForName);
 
 export {
-    // handleSubmit,
-    checkForName,
     getMeaning,
     babelPolyfill,
 };
